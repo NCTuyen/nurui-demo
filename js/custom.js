4cad8cf7cf854d834fc25843672dc5cb5e5d99bb
NCTuyen <congtuyenngo51@gmail.com>
2021-10-06 23:02:57 +0700

    upload code

---
+++ js/custom.js
@@ -0,0 +1,30 @@
+function openDots () {
+    var dot = document.getElementById ('openDots')
+    dot.classList.toggle ('open-dots')
+}
+function openSearch () {
+    var search = document.getElementById ('openSearch')
+    search.classList.toggle ('search-open')
+}
+function openMenu () {
+    var menu = document.getElementById ('openMenu')
+    menu.classList.toggle ('menu-open')
+}
+function slide() {
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:1
+            }
+        }
+    })
+}
